5c99cf3dd6150933d0dfaa29e1ce53f6f694aeeb
bidiazieu2024-hub <bidiaz.ieu2024@student.ie.edu>
2025-12-13 19:18:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 
-# Explicitly tell Flask where templates and static files are
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
 @app.route('/')
@@ -8,5 +7,4 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    # Flask runs on port 8080 in Cloud Run
     app.run(host='0.0.0.0', port=8080, debug=True)
